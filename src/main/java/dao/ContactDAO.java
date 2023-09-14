ae473d90abf4b6f5d62b647b506ed9d7b02e9b5d
Jeen <eyungenius@gmail.com>
2023-09-14 17:35:25 +0900

    dao modified

--- src/main/java/dao/ContactDAO.java
+++ src/main/java/dao/ContactDAO.java
@@ -14,55 +14,43 @@ import dto.ContactDTO;
 
 public class ContactDAO {
 	private static ContactDAO instance;
-	   
-	   private ContactDAO() {};
-	   
-	   
-	   public static ContactDAO getInstance(){
-	      if(instance == null) {
-	         instance = new ContactDAO();
-	      }
-	      return instance;
-	   }
 
-	   private Connection getConnection() throws Exception{
-	      // TODO Auto-generated method stub
-	      Context ctx = new InitialContext();
+	private ContactDAO() {};
+	public static ContactDAO getInstance() {
+		if (instance == null) {
+			instance = new ContactDAO();
+		}
+		return instance;
+	}
 
-	      DataSource ds = (DataSource)ctx.lookup("java:comp/env/jdbc/mysql");
-	      return ds.getConnection();
-	   }
-	
+	private Connection getConnection() throws Exception {
+		Context ctx = new InitialContext();
+
+		DataSource ds = (DataSource) ctx.lookup("java:comp/env/jdbc/mysql");
+		return ds.getConnection();
+	}
+
 	public List<ContactDTO> selectAll() throws Exception {
 		List<ContactDTO> list = new ArrayList<>();
 		String sql = "select * from contact";
-		try(
-				Connection con = this.getConnection();
+		try (Connection con = this.getConnection();
 				PreparedStatement pst = con.prepareStatement(sql);
-				ResultSet rs = pst.executeQuery();
-				){
-			while(rs.next()){
-				ContactDTO dto = new ContactDTO();
+				ResultSet rs = pst.executeQuery();) {
+			while (rs.next()) {
 				int seq = rs.getInt("seq");
 				String name = rs.getString("name");
-				String contact = rs.getString("contact");
-				dto.setSeq(seq);
-				dto.setName(name);
-				dto.setContact(contact);
-				list.add(dto);
+				String contact = rs.getString("contact");	
+				list.add(new ContactDTO(seq, name, contact));
 			}
 		}
 		return list;
 	}
-	
+
 	public int insertContact(String name, String contact) throws Exception {
 		String sql = "insert into contact values(0,?,?)";
-		try(
-				Connection con = this.getConnection();
-				PreparedStatement pst = con.prepareStatement(sql);
-				){
-			pst.setString(1,name);
-			pst.setString(2,contact);
+		try (Connection con = this.getConnection(); PreparedStatement pst = con.prepareStatement(sql);) {
+			pst.setString(1, name);
+			pst.setString(2, contact);
 			return pst.executeUpdate();
 		}
 	}
